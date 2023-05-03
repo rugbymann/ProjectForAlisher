39c1653abed757f00afc05940811387f3e6d4aab
Orkhan Aliyev <rugbisto@gmail.com>
2023-05-03 20:32:41 +0200

    add greets

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("How was your day?");
         System.out.println("Can i actually add new sout here?");
+        System.out.println("I think its work, see you again and thank you for your support");
     }
 }
